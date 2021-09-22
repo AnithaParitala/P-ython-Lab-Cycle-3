c09edfb05c8fa94abadff427be12defc00e54ec7
AnithaParitala <79572950+AnithaParitala@users.noreply.github.com>
2021-09-22 15:22:09 +0530

    Add files via upload

---
+++ Substring.python.py
@@ -0,0 +1,6 @@
+String=input("Enter string:")
+Sub_str=input("Enter word:")
+if(String.find(Sub_str)== -1):
+      print("Substring not found in string!")
+else:
+        print("Substring in string!")
